d17a85b9fcae969bb5f7c9791e82df46d76e15e4
john-walks-slow <john.ren.contact@gmail.com>
2021-11-25 14:33:42 +0800

    :sparkles: Add dynamic density

--- public/script.js
+++ public/script.js
@@ -3,9 +3,11 @@ const WINDOW_W = window.innerWidth || document.body.clientWidth;
 const WINDOW_H = window.innerHeight || document.body.clientHeight;
 const WINDOW_CENTER_X = WINDOW_W / 2;
 const WINDOW_CENTER_Y = WINDOW_H / 2;
-const PLAYER_TARGET_W = Math.min(WINDOW_W /9, WINDOW_H /9 / 44 * 37);
-const PLAYER_TARGET_H = Math.min(WINDOW_W /9, WINDOW_H /9 / 44 * 37) / 37 * 44;
-const OBJECT_W = { XL: PLAYER_TARGET_H * 1.5, L: PLAYER_TARGET_H * 1.2, M: PLAYER_TARGET_H, S: PLAYER_TARGET_W, XS: PLAYER_TARGET_W * 0.8 }
+// const PLAYER_TARGET_W = Math.min(WINDOW_W / 9, WINDOW_H / 9 / 44 * 37);
+// const PLAYER_TARGET_H = Math.min(WINDOW_W / 9, WINDOW_H / 9 / 44 * 37) / 37 * 44;
+const PLAYER_TARGET_W = Math.min(WINDOW_W / 15, WINDOW_H / 15 / 44 * 37);
+const PLAYER_TARGET_H = Math.min(WINDOW_W / 15, WINDOW_H / 15 / 44 * 37) / 37 * 44;
+const OBJECT_W = { XL: PLAYER_TARGET_H * 1.8, L: PLAYER_TARGET_H * 1.3, M: PLAYER_TARGET_H*0.9, S: PLAYER_TARGET_H * 0.7, XS: PLAYER_TARGET_H * 0.4 }
 // density: target object count in a circle (r=OBJECT_M_W*8)
 const DIALOG_HEIGHT = WINDOW_H / 3.5;
 const TEXT_PADDING_W = Math.min(WINDOW_W / 15, 50);
@@ -20,43 +22,20 @@ const FONT_FAMILY = "pixel"
 const FONT_FAMILY_HEADER = "pixel"
 // const TIME_DELAY = 60 * 60 * 1000;
 const TIME_DELAY = 0;
-const RANDOM_ZONE_W = OBJECT_W.M * 2;
-const OBJECT_DENSITY = 0.05;
-const DENSITY_OFFSET = OBJECT_W.M;
-const MOVE_SPEED = PLAYER_TARGET_W*1.3;
+const RANDOM_ZONE_W = OBJECT_W.XL;
+var DENSITY_OFFSET = OBJECT_W.M * 2;
+const MOVE_SPEED = PLAYER_TARGET_W;
 const ZONE_SIZE = MOVE_SPEED * 30;
 
-function createTestObject(object) {
-  object.id = 3;
-  // object.seed = [Math.random() * 360, Math.random() ** 0.5];
-  object.seed = [Math.random() * 360, Math.random()**0.5];
+const timestamp = Date.parse(new Date());
 
-  // let rad = object.seed[0] * (Math.PI / 180);
-  // let distance = object.seed[1] / 100 * RANDOM_ZONE_W;
-  // let oY = Math.cos(rad) * distance;
-  // let oX = Math.sin(rad) * distance;
-  // let isOver = false;
-  // for (let index = 0; index < objectList.length; index++) {
-  // 	const oo = objectList[index];
-  // 	let rad = oo.seed[0] * (Math.PI / 180);
-  //     let distance = oo.seed[1] / 100 * RANDOM_ZONE_W;
-  //     let ooY = Math.cos(rad) * distance;
-  //     let ooX = Math.sin(rad) * distance;
-  //     if (Math.abs(ooY-oY)*2<(OBJECT_W[object.size]+OBJECT_W[oo.size] || Math.abs(ooX-oX)*2<(OBJECT_W[object.size]+OBJECT_W[oo.size]))){
-  // 		isOver = true;
-  //     }
-  // 	console.log(Math.abs(ooY-oY)*2,OBJECT_W[object.size],OBJECT_W[oo.size]);
-  // };
-  // // console.log(isOver);
-  // if (isOver) {object = createTestObject(object)};
+
+function createTestObject(object) {
+  object._id = 3;
+  object.seed = [Math.random() * 360, Math.random() ** 0.5];
   return object;
 }
-function createObject(object) {
-  object.id = (Number(this.objectList[this.objectList.length - 1].id) + 1).toString();
-  object.seed = [Math.random() * 360, Math.random() * 100];
-  console.log(object);
-  return object;
-}
+
 function vectorAngle(x, y) {
   let mX = Math.sqrt(x.reduce((acc, n) => acc + Math.pow(n, 2), 0));
   let mY = Math.sqrt(y.reduce((acc, n) => acc + Math.pow(n, 2), 0));
@@ -154,31 +133,44 @@ class LoadingScene extends Phaser.Scene {
       this.load.image('bg', 'assets/bg.png');
       this.load.image('dialog', 'assets/dialog.png');
       this.load.spritesheet('player', 'assets/White.png', { frameWidth: 37, frameHeight: 44 });
-      // [...Array(1)].forEach(() => {
+      // [...Array(10000)].forEach(() => {
       //   objectList.push(createTestObject({
-      //     "birthday": 1637224309,
+      //     "birthday": 1637819836697,
       //     "movement": "static",
       //     "size": "L",
+      //     "columns": 18,
+      //     "rows": 18,
+      //     "zFactor": 1,
+      //     "isAnimate": false,
       //     "dialog": "我是量产机1号。\n量产机只有我。\n我曾经统治过世界！\n你不信？我可是作者钦定的。你没发现我长得和你很像吗？\n写代码不容易，一直在查来查去解决琐碎的问题，会有积累吗？".split("\n"),
       //     "link": null
       //   }))
       // });
-      objectList.forEach((o) => { this.load.image("object" + o._id, 'assets/objects/' + o._id + '.png') });
-      // console.log(objectList);
+      objectList.forEach((o) => {
+        if (timestamp - o.birthday < (48 * 60 * 60)) { DENSITY_OFFSET -= 10; }
+        switch (o.isAnimate) {
+          case true:
+            this.load.spritesheet("object" + o._id, 'assets/objects/' + o._id + '.png', { frameWidth: o.columns, frameHeight: o.rows });
+            break;
+          default:
+            this.load.image("object" + o._id, 'assets/objects/' + o._id + '.png')
+            break;
+        }
+      });
+      DENSITY_OFFSET = Math.min(OBJECT_W.L, DENSITY_OFFSET);
       this.load.on("complete", () => {
         console.log(objectList);
         this.scene.start("MainScene", { "objectList": objectList });
         this.scene.stop("LoadingScene");
       }, this);
-      this.load.on('progress',(progress)=>{
+      this.load.on('progress', (progress) => {
         console.log(progress);
-        this.label.text ='Now Loading ' + ".".repeat(Math.round(progress*15));
+        this.label.text = 'Now Loading ' + ".".repeat(Math.round(progress * 15));
       })
       this.load.start();
     })
-    this.add.rectangle(WINDOW_CENTER_X , WINDOW_CENTER_Y, WINDOW_W, WINDOW_H, 0xFFFFFF);
-    this.label = this.add.text(30 , WINDOW_CENTER_Y,'Now Loading ',{align:"center",color:"#000000",fontSize:WINDOW_W/20})
-    // Connect to the `http://feathers-api.com/messages` service
+    this.add.rectangle(WINDOW_CENTER_X, WINDOW_CENTER_Y, WINDOW_W, WINDOW_H, 0xFFFFFF);
+    this.label = this.add.text(30, WINDOW_CENTER_Y, 'Now Loading ', { align: "center", color: "#000000", fontSize: WINDOW_W / 20 })
   }
   update() {
 
@@ -243,22 +235,30 @@ class MainScene extends Phaser.Scene {
     this.objectMap = [];
     this.objectList = this.objectList.reverse();
     this.objects = this.physics.add.group();
-    let timestamp = Date.parse(new Date());
     let previousDate = timestamp;
     let offset;
-    let dateOffset = (PLAYER_TARGET_H + OBJECT_W.XL) / 2 + 5;
+    let dateOffset = (PLAYER_TARGET_H + OBJECT_W.L) / 2 + 5;
     this.objectList.forEach((o, i) => {
       if (timestamp - o.birthday < TIME_DELAY) { return; }
-      dateOffset += Math.min(1, (30 * 24 * 60 * 60)/(previousDate - o.birthday)) * 5 * PLAYER_TARGET_W;
-      offset = (DENSITY_OFFSET * i) **0.6;
-      console.log({dateOffset,offset});
+      dateOffset += Math.min(1, (previousDate - o.birthday) / (14 * 24 * 60 * 60)) * 5 * PLAYER_TARGET_W;
+      previousDate = o.birthday;
+      offset = (DENSITY_OFFSET * (i ** 0.5));
+      // console.log({ dateOffset, offset });
       // console.log(Math.min(1, (previousDate - o.birthday) / (30 * 24 * 60 * 60)));
       let rad = o.seed[0] * (Math.PI / 180);
       let distance = o.seed[1] * RANDOM_ZONE_W + offset + dateOffset;
       o.x = Math.cos(rad) * distance;
       o.y = Math.sin(rad) * distance;
-      o.zFactor = o.zFactor || 1;
+      o.zFactor = (o.zFactor == 1) ? o.zFactor - 0.1 + Math.random() * 0.2 : o.zFactor;
+      o.ratio = o.rows / o.columns;
       o.zone = [Math.ceil(o.x / ZONE_SIZE), Math.ceil(o.y / ZONE_SIZE)]
+      o.isAnimate ? this.anims.create({
+        key: 'spritesheet' + o._id,
+        frames: 'object' + o._id,
+        frameRate: 2,
+        repeat: -1,
+        repeatDelay: 0
+      }) : false;
       if (!this.objectMap[o.zone[0]]) {
         this.objectMap[o.zone[0]] = []
       }
@@ -419,11 +419,11 @@ class MainScene extends Phaser.Scene {
     }
     let moveX = (x) => {
       this.player.setVelocityX(x);
-      this.objects.children.each((o) => { o.setVelocityX(x * (o.data.values.zFactor - 1)) })
+      this.objects.children.each((o) => { o.setVelocityX(- x * (o.data.values.zFactor - 1)) })
     }
     let moveY = (y) => {
       this.player.setVelocityY(y);
-      this.objects.children.each((o) => { o.setVelocityY(y * (o.data.values.zFactor - 1)) })
+      this.objects.children.each((o) => { o.setVelocityY(- y * (o.data.values.zFactor - 1)) })
     }
 
     let updateObjects = (previousZone, currentZone) => {
@@ -443,9 +443,15 @@ class MainScene extends Phaser.Scene {
           o.instance = this.objects.create(o.x, o.y, "object" + o._id);
           // o.instance = this.physics.add.sprite(o.x,o.y,"object"+o.id);
           // console.log(this.objects);
+          o.instance.depth = o.zFactor;
           o.instance.setData("dialog", o.dialog);
           o.instance.setData("zFactor", o.zFactor);
-          o.instance.setDisplaySize(OBJECT_W[o.size], OBJECT_W[o.size]);
+          o.ratio < 1 ?
+            o.instance.setDisplaySize(OBJECT_W[o.size] / o.zFactor, OBJECT_W[o.size] * o.ratio / o.zFactor)
+            : o.instance.setDisplaySize(OBJECT_W[o.size] / o.zFactor / o.ratio, OBJECT_W[o.size] / o.zFactor);
+          if (o.isAnimate) {
+            o.instance.anims.play('spritesheet' + o._id)
+          }
           o.instance.refreshBody();
           // this.objects.add(o.instance);
           this.physics.add.collider(this.player, o.instance, this.colliderHandler);
