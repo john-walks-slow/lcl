60cb23970dc7ccef1d5e1f5256b41e585c38029b
john-walks-slow <john.ren.contact@gmail.com>
2021-11-25 16:31:47 +0800

    :bug: Update used colors when reset

--- react_page/src/components/Reset.jsx
+++ react_page/src/components/Reset.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { resetGrid } from '../store/actions/actionCreators';
+import { resetGrid, updateUsedColors } from '../store/actions/actionCreators';
 
 const Reset = ({ resetGridDispatch }) => (
   <button type="button" className="reset" onClick={resetGridDispatch}>
@@ -9,7 +9,7 @@ const Reset = ({ resetGridDispatch }) => (
 );
 
 const mapDispatchToProps = dispatch => ({
-  resetGridDispatch: () => dispatch(resetGrid())
+  resetGridDispatch: () => {dispatch(resetGrid());dispatch(updateUsedColors())}
 });
 
 const ResetContainer = connect(
