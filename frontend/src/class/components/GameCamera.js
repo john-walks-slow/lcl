dc1665edc1e6f827240265dfad0525f86eee2bce
john-walks-slow <john.ren.contact@gmail.com>
2021-12-16 19:58:57 +0800

    :bug: 连续点击map相机卡死

--- frontend/src/class/components/GameCamera.js
+++ frontend/src/class/components/GameCamera.js
@@ -15,19 +15,6 @@ export default class GameCamera extends Phaser.Cameras.Scene2D.Camera {
       duration: 1000,
       completeDelay: 0,
     })
-    this.zoomInAnim = this.scene.tweens.create({
-      targets: this,
-      props: { zoom: configurations.ZOOM_LEVEL, alpha: 1 },
-      ease: 'Cubic', // 'Cubic', 'Elastic', 'Bounce', 'Back'
-      duration: 800,
-      completeDelay: 0,
-    })
-    this.zoomOutAnim = this.scene.tweens.create({
-      targets: this,
-      props: { zoom: configurations.ZOOM_OUT_LEVEL, alpha: 0.6 },
-      ease: 'Cubic', // 'Cubic', 'Elastic', 'Bounce', 'Back'
-      duration: 1000,
-    })
     this.setDisplay()
     this.fadeIn()
     this.setZoom(configurations.ZOOM_OUT_LEVEL)
@@ -38,10 +25,36 @@ export default class GameCamera extends Phaser.Cameras.Scene2D.Camera {
     this.setSize(configurations.WINDOW_W, configurations.WINDOW_H)
   }
   zoomIn() {
+    if (this.zoomInAnim && this.zoomInAnim.isPlaying()) {
+      return
+    }
+    if (this.zoomOutAnim && this.zoomOutAnim.isPlaying()) {
+      this.zoomOutAnim.stop()
+    }
+    this.zoomInAnim = this.scene.tweens.create({
+      targets: this,
+      props: { zoom: configurations.ZOOM_LEVEL, alpha: 1 },
+      ease: 'Cubic', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+      duration: 800,
+      completeDelay: 0,
+    })
     this.zoomInAnim.play()
     this.scene.setZoomed(false)
   }
   zoomOut() {
+    if (this.zoomOutAnim && this.zoomOutAnim.isPlaying()) {
+      return
+    }
+    if (this.zoomInAnim && this.zoomInAnim.isPlaying()) {
+      this.zoomInAnim.stop()
+    }
+    this.zoomOutAnim = this.scene.tweens.create({
+      targets: this,
+      props: { zoom: configurations.ZOOM_OUT_LEVEL, alpha: 0.6 },
+      ease: 'Cubic', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+      duration: 1000,
+    })
+
     this.zoomOutAnim.play()
     this.scene.setZoomed(true)
   }
@@ -51,7 +64,7 @@ export default class GameCamera extends Phaser.Cameras.Scene2D.Camera {
     // this.initAnim.stop(0);
     // let result = "unchanged";
     this.zoom == configurations.ZOOM_LEVEL && this.zoomOut()
-    this.zoom == configurations.ZOOM_OUT_LEVEL && this.zoomIn()
+    this.zoom < configurations.ZOOM_LEVEL && this.zoomIn()
     // return result;
   }
 }
